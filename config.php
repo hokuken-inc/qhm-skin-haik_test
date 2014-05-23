68729fca59dba2eed8fa89f87945149e78fc142b
masamori <r.masamori@gmail.com>
2014-05-23 12:35:35 +0900

    Add paint and select

--- config.php
+++ config.php
@@ -11,19 +11,22 @@ return array(
         'heading_color' => array(
             'title' => 'ヘッダー文字色',
             'description' => 'ヘッダーの文字色に指定する色です。',
-            'type' => 'color',
-            'value' => 'green'
+            'type'        => 'color',
+            'palette'     => array('white', 'green', 'gray'), 
+            'value'       => 'green'
         ),
         'body_color' => array(
             'title' => '本文文字色',
             'description' => '本文の文字色に指定する色です。',
             'type' => 'color',
+            'palette'     => array('white', 'green', 'gray'), 
             'value' => 'black'
         ),
         'body_font' => array(
             'title' => '本文フォント',
             'description' => '本文のフォント指定です。',
-            'type' => 'font',
+            'type' => 'select',
+            'options' => array('sans-serif'),
             'value' => 'sans-serif'
         ),
         'body_bgimage' => array(
@@ -37,6 +40,21 @@ return array(
             'description' => 'ロゴ画像を表示するかどうか。',
             'type' => 'if',
             'value' => true
-        )
+        ),
+        'theme_color' => array(
+            'title' => 'テーマカラー',
+            'description' => '本文の文字色に指定する色です。',
+            'type' => 'color',
+            'palette'  => array('#428bca', '#5cb85c', '#5bc0de', '#f0ad4e', '#d9534f'), 
+            'value' => '#5cb85c'
+        ),
+        'theme_if' => array(
+            'title' => '背景と文字の色',
+            'description' => 'true:白背景文字,false：色背景白文字',
+            'type'    => 'if',
+            'paint'   => 'theme_color',
+            'value'   =>  true
+        ),
+        
     )
 );
